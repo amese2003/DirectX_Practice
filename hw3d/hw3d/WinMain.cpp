0576861fc5122dcdce0bbc8e8b10d904254dc55e
Yong Ju Kwak <amese1225@gmail.com>
2020-12-07 22:15:46 +0900

    윈도우창 출력후, 종료시 프로세스 제거

--- hw3d/hw3d/WinMain.cpp
+++ hw3d/hw3d/WinMain.cpp
@@ -1,5 +1,18 @@
 #include <Windows.h>
 
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+	switch (msg)
+	{
+		case WM_CLOSE:
+			PostQuitMessage(69);
+			break;
+		default:
+			break;
+	}
+	
+	return DefWindowProc(hWnd, msg, wParam, lParam);
+}
+
 int CALLBACK WinMain(
 	HINSTANCE hInstance,
 	HINSTANCE hPrevInstance,
@@ -12,7 +25,7 @@ int CALLBACK WinMain(
 	WNDCLASSEX wc = { 0 };
 	wc.cbSize = sizeof(wc);
 	wc.style = CS_OWNDC;
-	wc.lpfnWndProc = DefWindowProc;
+	wc.lpfnWndProc = WndProc;
 	wc.cbClsExtra = 0;
 	wc.cbWndExtra = 0;
 	wc.hInstance = hInstance;
@@ -34,6 +47,19 @@ int CALLBACK WinMain(
 
 	ShowWindow(hWnd, SW_SHOW);
 
-	while (true);
+	MSG msg;
+	BOOL gResult;
+
+	while ((gResult = GetMessage(&msg, nullptr, 0, 0)) > 0) {
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	if (gResult == -1)
+		return -1;
+	else {
+		return msg.wParam;
+	}
+
 	return 0;
 }
