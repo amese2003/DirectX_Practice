5ef7e7df4d7942b15af620f27fff3d667e15bc73
Yong Ju Kwak <amese1225@gmail.com>
2020-12-07 21:13:34 +0900

    윈도우 생성

--- 새 폴더/hw3d/hw3d/WinMain.cpp
+++ 새 폴더/hw3d/hw3d/WinMain.cpp
@@ -6,8 +6,34 @@ int CALLBACK WinMain(
 	LPSTR     lpCmdLine,
 	int       nCmdShow
 ) {
-	while (true) {
+	const auto pClassName = "hw3dbutts";
 
-	}
+	//윈도우 클래스 등록
+	WNDCLASSEX wc = { 0 };
+	wc.cbSize = sizeof(wc);
+	wc.style = CS_OWNDC;
+	wc.lpfnWndProc = DefWindowProc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hInstance = hInstance;
+	wc.hIcon = nullptr;
+	wc.hCursor = nullptr;
+	wc.hbrBackground = nullptr;
+	wc.lpszMenuName = nullptr;
+	wc.lpszClassName = pClassName;
+	wc.hIconSm = nullptr;
+	RegisterClassEx(&wc);
+
+	HWND hWnd = CreateWindowEx(
+		0, pClassName,
+		"Happy Hard Window",
+		WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
+		200, 200, 640, 480,
+		nullptr, nullptr, hInstance, nullptr
+	);
+
+	ShowWindow(hWnd, SW_SHOW);
+
+	while (true);
 	return 0;
 }
