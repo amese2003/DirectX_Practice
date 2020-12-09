7b0ac44a6630674247db3f72c6585f21455fe462
Yong Ju Kwak <amese1225@gmail.com>
2020-12-09 20:59:12 +0900

    윈도우 프레임 워크, 윈도우 창을 WinMain에서 헤더파일과 cpp로 분할

---
+++ hw3d/hw3d/Window.cpp
@@ -0,0 +1,94 @@
+#include "Window.h"
+
+
+Window::WindowClass Window::WindowClass::wndClass;
+
+Window::WindowClass::WindowClass() noexcept
+	:
+	hInst(GetModuleHandle(nullptr))
+{
+	WNDCLASSEX wc = { 0 };
+	wc.cbSize = sizeof(wc);
+	wc.style = CS_OWNDC;
+	wc.lpfnWndProc = HandleMsgSetup;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hInstance = GetInstance();
+	wc.hIcon = nullptr;
+	wc.hCursor = nullptr;
+	wc.hbrBackground = nullptr;
+	wc.lpszMenuName = nullptr;
+	wc.lpszClassName = GetName();
+	wc.hIconSm = nullptr;
+	RegisterClassEx(&wc);
+}
+
+Window::WindowClass::~WindowClass() {
+	UnregisterClass(wndClassName, GetInstance());
+}
+
+const char* Window::WindowClass::GetName() noexcept {
+	return wndClassName;
+}
+
+HINSTANCE Window::WindowClass::GetInstance() noexcept {
+	return wndClass.hInst;
+}
+
+Window::Window(int width, int height, const char* name) noexcept {
+	RECT wr;
+
+	wr.left = 100;
+	wr.right = width + wr.left;
+	wr.top = 100;
+	wr.bottom = height + wr.top;
+	AdjustWindowRect(&wr, WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU, FALSE);
+
+	hWnd = CreateWindow(
+		WindowClass::GetName(), name,
+		WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
+		CW_USEDEFAULT, CW_USEDEFAULT, wr.right - wr.left, wr.bottom - wr.top,
+		nullptr, nullptr, WindowClass::GetInstance(), this
+	);
+	ShowWindow(hWnd, SW_SHOWDEFAULT);
+}
+
+Window::~Window() {
+	DestroyWindow(hWnd);
+}
+
+LRESULT WINAPI Window::HandleMsgSetup(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) noexcept {
+	if (msg == WM_NCCREATE) {
+
+		//생성 데이터에서 윈도우 클래스로 PTR 추출
+		const CREATESTRUCTW* const pCreate = reinterpret_cast<CREATESTRUCTW*>(lParam);
+		Window* const pWnd = static_cast<Window*>(pCreate->lpCreateParams);
+
+		//윈도우 인스턴스에 ptr를 저장하도록 WinAPI 사용자 데이터 설정
+		SetWindowLongPtr(hWnd, GWLP_USERDATA, reinterpret_cast<LONG_PTR>(pWnd));
+		// 설정이 끝났으니 메시지 proc을 일반 (non-setUP) 처리로 변경
+		SetWindowLongPtr(hWnd, GWLP_WNDPROC, reinterpret_cast<LONG_PTR>(&Window::HandleMsgThunk));
+		// 창 인스턴스 핸들러로 메시지 투척
+		return pWnd->HandleMsg(hWnd, msg, wParam, lParam);
+	}
+	return DefWindowProc(hWnd, msg, wParam, lParam);
+}
+
+LRESULT WINAPI Window::HandleMsgThunk(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) noexcept {
+	//윈도우 인스턴스에 대한 ptr 검색
+	Window* const pWnd = reinterpret_cast<Window*>(GetWindowLongPtr(hWnd, GWLP_USERDATA));
+
+	//윈도우 인스턴스 핸들러로 메시지 전달
+	return pWnd->HandleMsg(hWnd, msg, wParam, lParam);
+}
+
+LRESULT Window::HandleMsg(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) noexcept {
+	switch (msg)
+	{
+	case WM_CLOSE:
+		PostQuitMessage(0);
+		return 0;
+	}
+
+	return DefWindowProc(hWnd, msg, wParam, lParam);
+}
