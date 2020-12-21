539b6fc08b3a2037777dd4b7f55af2c06a9f46b7
Yong Ju Kwak <amese1225@gmail.com>
2020-12-21 15:16:54 +0900

    정적 index Buffer가 transform이 하나 이상이면 에러가 걸리던 문제 해결

--- hw3d/hw3d/DrawableBase.h
+++ hw3d/hw3d/DrawableBase.h
@@ -20,6 +20,17 @@ public:
 		staticBinds.push_back(std::move(ibuf));		
 	}
 
+	void SetIndexFromStatic() noexcept(!IS_DEBUG) {
+		assert("Attempting to add index buffer a second time" && pIndexBuffer == nullptr);
+		for (const auto& b : staticBinds) {
+			if (const auto p = dynamic_cast<IndexBuffer*>(b.get())) {
+				pIndexBuffer = p;
+				return;
+			}
+		}
+		assert("Failed to find index buffer in static binds" && pIndexBuffer != nullptr);
+	}
+
 private:
 	const std::vector<std::unique_ptr<Bindable>>& GetStaticBinds() const noexcept {
 		return staticBinds;
