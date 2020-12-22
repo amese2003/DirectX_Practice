f849e64a5bedb1723e22282a632810d2f33a6784
Yong Ju Kwak <amese1225@gmail.com>
2020-12-22 17:00:59 +0900

    GdiPlus가 초기화를 하지않아서 불러오지지 않은 문제 수정, plane에 텍스쳐 warp

--- hw3d/hw3d/App.cpp
+++ hw3d/hw3d/App.cpp
@@ -8,6 +8,10 @@
 #include <iterator>
 #include "Surface.h"
 #include "GDIPlusManager.h"
+#include "Sheet.h"
+
+
+GDIPlusManager gdipm;
 
 App::App()
 	:
@@ -39,6 +43,11 @@ App::App()
 					gfx, rng, adist, ddist,
 					odist, rdist, longdist, latdist
 					);
+			case 3:
+				return std::make_unique<Sheet>(
+					gfx, rng, adist, ddist,
+					odist, rdist
+					);
 			default:
 				assert(false && "bad drawable type in factory");
 				return {};
@@ -54,15 +63,12 @@ App::App()
 		std::uniform_real_distribution<float> bdist{ 0.4f,3.0f };
 		std::uniform_int_distribution<int> latdist{ 5,20 };
 		std::uniform_int_distribution<int> longdist{ 10,40 };
-		std::uniform_int_distribution<int> typedist{ 0,2 };
+		std::uniform_int_distribution<int> typedist{ 0,3 };
 	};
 
 	Factory f(wnd.Gfx());
 	drawables.reserve(nDrawables);
 	std::generate_n(std::back_inserter(drawables), nDrawables, f);
-
-	const auto s = Surface::FromFile("Images\\40kon.png");
-
 
 	wnd.Gfx().SetProjection(DirectX::XMMatrixPerspectiveLH(1.0f, 3.0f / 4.0f, 0.5f, 40.0f));
 }
@@ -72,7 +78,7 @@ void App::DoFrame() {
 	wnd.Gfx().ClearBuffer(0.07f, 0.0f, 0.12f);
 	for (auto& d : drawables)
 	{
-		d->Update(dt);
+		d->Update(wnd.kbd.KeyIsPressed(VK_SPACE) ? 0.0f : dt);
 		d->Draw(wnd.Gfx());
 	}
 	wnd.Gfx().EndFrame();
