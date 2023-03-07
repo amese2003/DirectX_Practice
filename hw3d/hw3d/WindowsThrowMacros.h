3715bbfe2318db7183f9ade518a0e81ae761940e
용주 곽 <amese1225@gmail.com>
2023-03-07 14:54:19 +0900

    프로젝트 전체 초기화

--- hw3d/hw3d/WindowsThrowMacros.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#define CUSTOM_EXCEPT( hr ) Window::HrException( __LINE__,__FILE__,hr )
-#define CUSTOM_LAST_EXCEPT() Window::HrException( __LINE__,__FILE__,GetLastError() )
-#define CUSTOM_NOGFX_EXCEPT() Window::NoGfxException( __LINE__,__FILE__ )
